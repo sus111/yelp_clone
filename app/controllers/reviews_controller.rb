acc0f780f8e4cdab690f96ddb2a1edb933ebb057
sus111 <sgraham535@gmail.com>
2016-08-25 14:55:01 +0100

    build with user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,7 +12,7 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.build_review review_params, current_user
+    @review = @restaurant.reviews.build_with_user(review_params, current_user)
     if @review.save
       redirect_to "/restaurants"
     else
@@ -23,17 +23,6 @@ class ReviewsController < ApplicationController
       end
     end
   end
-
-  # def create
-  #   @restaurant = Restaurant.find(params[:restaurant_id])
-  #   @review = @restaurant.reviews.new(review_params)
-  #   @review.user = current_user
-  #   if @review.save
-  #     redirect_to "/restaurants"
-  #   else
-  #     render "new"
-  #   end
-  # end
 
   private
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,10 +7,4 @@ class Restaurant < ApplicationRecord
 
   validates :name, length: { minimum: 3 }, uniqueness: true
 
-  def build_review(attributes = {}, user)
-    review = reviews.build(attributes)
-    review.user = user
-    review
-  end
-
 end
