6447bb461be06c140b734e6be3c87a25c5f6a131
sus111 <sgraham535@gmail.com>
2016-08-26 14:32:52 +0100

    add endorsements

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -5,7 +5,8 @@ class EndorsementsController < ApplicationController
   def create
     @review = Review.find(params[:review_id])
     @review.endorsements.create
-    redirect_to '/restaurants'
+    render json: {new_endorsement_count: @review.endorsements.count}
+    # redirect_to '/restaurants'
   end
 
 end
--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -7,15 +7,10 @@ feature 'endorsing reviews' do
     leave_review("tasty", 4)
   end
 
-  scenario 'a user can endorse a review which updates the review endorsement count' do
-    visit '/restaurants'
-    click_link 'Endorse Review'
-    expect(page).to have_content('1 endorsement')
-  end
-
   it 'a user can endorse a review, which increments the endorsement count', js: true do
     visit '/restaurants'
     click_link 'Endorse Review'
+    save_and_open_page
     expect(page).to have_content("1 endorsement")
   end
 end
