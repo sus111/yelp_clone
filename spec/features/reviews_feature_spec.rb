d02ddc52f2be2929ac4f2615f7949231d2ff05d9
sus111 <sgraham535@gmail.com>
2016-08-26 12:00:30 +0100

    add stars

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -13,17 +13,16 @@ feature "reviewing" do
 
   scenario "allows users to enter a restaurant review" do
     leave_review('So so', '3')
-
     expect(current_path).to eq "/restaurants"
     expect(page).to have_content "So so"
   end
 
-scenario 'displays an average rating for all reviews' do
-  leave_review('So so', '3')
-  click_link "Sign out"
-  sign_up(user: "dan@example.com", password: "daniel")
-  leave_review('Great', '5')
-  expect(page).to have_content('Average rating: 4')
-end
+  scenario 'displays an average rating for all reviews' do
+    leave_review('so so', '3')
+    click_link "Sign out"
+    sign_up(user: "dan@example.com", password: "daniel")
+    leave_review('Great!', '5')
+    expect(page).to have_content('Average rating: ★★★★☆')
+  end
 
 end
